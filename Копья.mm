51b9609940761b41c68c9f64ef345ec70f51a66f
Kreyl <laystoll@yandex.ru>
2019-11-12 23:52:23 +0300

    Working on Antenna

---
+++ Копья.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x41a;&#x43e;&#x43f;&#x44c;&#x44f;" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1572992929971"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="bubble">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode TEXT="OK">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode TEXT="NotOk">
+<icon BUILTIN="button_cancel"/>
+</stylenode>
+<stylenode TEXT="Question">
+<icon BUILTIN="help"/>
+</stylenode>
+<stylenode TEXT="Attention">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Stopper">
+<icon BUILTIN="stop-sign"/>
+</stylenode>
+<stylenode TEXT="Waiting">
+<icon BUILTIN="hourglass"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" BACKGROUND_COLOR="#ccffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" BACKGROUND_COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" BACKGROUND_COLOR="#ffcccc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="&#x42d;&#x43b;&#x435;&#x43a;&#x442;&#x440;&#x43e;&#x43d;&#x438;&#x43a;&#x430;" POSITION="right" ID="ID_256711269" CREATED="1572992962692" MODIFIED="1572992968840">
+<node TEXT="&#x424;&#x443;&#x43d;&#x43a;&#x446;&#x438;&#x43e;&#x43d;&#x430;&#x43b;" ID="ID_1194818579" CREATED="1572993045680" MODIFIED="1572993050708">
+<node TEXT="WS2812" ID="ID_1397944794" CREATED="1572992969686" MODIFIED="1572992980503"/>
+<node TEXT="&#x410;&#x43a;&#x441;&#x435;&#x43b;&#x435;&#x440;&#x43e;&#x43c;&#x435;&#x442;&#x440;/&#x433;&#x438;&#x440;&#x43e;&#x441;&#x43a;&#x43e;&#x43f;" ID="ID_706726704" CREATED="1572992985411" MODIFIED="1572992996202"/>
+<node TEXT="&#x41a;&#x43d;&#x43e;&#x43f;&#x43a;&#x430;" ID="ID_339351522" CREATED="1572992996968" MODIFIED="1572993001680">
+<node TEXT="&#x414;&#x432;&#x435;" ID="ID_514798527" CREATED="1572993002375" MODIFIED="1572993004506"/>
+</node>
+<node TEXT="&#x417;&#x430;&#x440;&#x44f;&#x434;&#x43a;&#x430; &#x430;&#x43a;&#x43a;&#x443;&#x43c;&#x430;" ID="ID_1062479042" CREATED="1572993028920" MODIFIED="1572993037718">
+<node TEXT="USB &#x440;&#x430;&#x437;&#x44a;&#x451;&#x43c;" ID="ID_518679779" CREATED="1572993061845" MODIFIED="1572993067677"/>
+</node>
+<node TEXT="&#x417;&#x432;&#x443;&#x43a;" STYLE_REF="NotOk" FOLDED="true" ID="ID_1211150738" CREATED="1572993178383" MODIFIED="1572993191558">
+<node TEXT="&#x41d;&#x435;&#x44f;&#x441;&#x43d;&#x43e; &#x437;&#x430;&#x447;&#x435;&#x43c;" ID="ID_1727854429" CREATED="1572993181276" MODIFIED="1572993200457"/>
+<node TEXT="&#x421;&#x438;&#x43b;&#x44c;&#x43d;&#x43e;&#x435; &#x443;&#x441;&#x43b;&#x43e;&#x436;&#x43d;&#x435;&#x43d;&#x438;&#x435;" ID="ID_797448527" CREATED="1572993200786" MODIFIED="1572993210779">
+<node TEXT="&#x42d;&#x43b;&#x435;&#x43a;&#x442;&#x440;&#x43e;&#x43d;&#x438;&#x43a;&#x438;" ID="ID_1582568041" CREATED="1572993211352" MODIFIED="1572993214580"/>
+<node TEXT="&#x41a;&#x43e;&#x440;&#x43f;&#x443;&#x441;&#x430;" ID="ID_97765746" CREATED="1572993215394" MODIFIED="1572993222859"/>
+</node>
+</node>
+<node TEXT="&#x41c;&#x43e;&#x449;&#x43d;&#x44b;&#x439; &#x441;&#x432;&#x435;&#x442;&#x43e;&#x434;&#x438;&#x43e;&#x434;" ID="ID_1250153808" CREATED="1572993228351" MODIFIED="1572993239599">
+<node TEXT="&#x414;&#x43b;&#x44f; &#x436;&#x435;&#x437;&#x43b;&#x430;" ID="ID_1487881896" CREATED="1572993246101" MODIFIED="1572993253441"/>
+<node TEXT="&#x410; &#x43d;&#x435; &#x442;&#x440;&#x438;?" ID="ID_1543746081" CREATED="1572993281104" MODIFIED="1572993285746">
+<node TEXT="&#x410; &#x43d;&#x435; &#x436;&#x438;&#x440;&#x43d;&#x43e;?" ID="ID_1519096064" CREATED="1572993290228" MODIFIED="1572993293450">
+<node TEXT="&#x414;&#x432;&#x430; &#x43f;&#x443;&#x442;&#x438;" ID="ID_1950964733" CREATED="1572993318507" MODIFIED="1572993323082">
+<node TEXT="&#x422;&#x43e;&#x43b;&#x44c;&#x43a;&#x43e; 3&#x445;&#x428;&#x418;&#x41c;" ID="ID_1701781026" CREATED="1572993324195" MODIFIED="1572993338240">
+<node TEXT="&#x418; &#x434;&#x430;&#x43b;&#x44c;&#x448;&#x435; &#x43f;&#x43b;&#x430;&#x442;&#x43e;&#x439; &#x434;&#x440;&#x430;&#x439;&#x432;&#x435;&#x440;&#x430;" ID="ID_1835941574" CREATED="1572993339518" MODIFIED="1572993348581"/>
+</node>
+<node TEXT="1&#x445; ST1CC40 + 3&#x445;&#x428;&#x418;&#x41c;" ID="ID_1094627410" CREATED="1572993350810" MODIFIED="1572993380815">
+<node TEXT="&#x41a;&#x43e;&#x43d;&#x43a;&#x440;&#x435;&#x442;&#x43d;&#x43e; &#x434;&#x43b;&#x44f; &#x436;&#x435;&#x437;&#x43b;&#x430;" ID="ID_1284327643" CREATED="1572993382667" MODIFIED="1572993389987"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="&#x41a;&#x43e;&#x440;&#x43e;&#x447;&#x435;, 3&#x445;&#x428;&#x418;&#x41c;, &#x430; ST1CC40 &#x43f;&#x43e; &#x432;&#x43e;&#x437;&#x43c;&#x43e;&#x436;&#x43d;&#x43e;&#x441;&#x442;&#x438;" ID="ID_1988285132" CREATED="1572993408712" MODIFIED="1572993429026"/>
+</node>
+</node>
+<node TEXT="&#x41a;&#x43e;&#x43c;&#x43f;&#x43e;&#x43d;&#x435;&#x43d;&#x442;&#x44b;" ID="ID_1469833657" CREATED="1572993080197" MODIFIED="1572993084864">
+<node TEXT="&#x41f;&#x440;&#x43e;&#x446;" ID="ID_1729859774" CREATED="1572993085672" MODIFIED="1572993087229">
+<node TEXT="&#x41d;&#x443;&#x436;&#x43d;&#x43e;" ID="ID_1554070689" CREATED="1572993459996" MODIFIED="1572993462816">
+<node TEXT="&#x41c;&#x43d;&#x43e;&#x433;&#x43e; &#x43f;&#x430;&#x43c;&#x44f;&#x442;&#x438;" ID="ID_1653089020" CREATED="1572993463549" MODIFIED="1572993471832"/>
+<node TEXT="&#x425;&#x43e;&#x440;&#x43e;&#x448;&#x43e; &#x431;&#x44b; M4" ID="ID_695065754" CREATED="1572993472726" MODIFIED="1572993482471">
+<node TEXT="&#x414;&#x43b;&#x44f; &#x433;&#x438;&#x440;&#x43e;&#x441;&#x43a;&#x43e;&#x43f;&#x430;" ID="ID_1600496039" CREATED="1572993483690" MODIFIED="1572993487531"/>
+</node>
+<node TEXT="SPIx2" ID="ID_1064652735" CREATED="1573057147483" MODIFIED="1573057153894"/>
+</node>
+<node TEXT="STM32L433CCT6" ID="ID_368927039" CREATED="1572994331673" MODIFIED="1572994333738"/>
+</node>
+<node TEXT="&#x417;&#x430;&#x440;&#x44f;&#x434;&#x43a;&#x430;" ID="ID_1580575250" CREATED="1572993087744" MODIFIED="1572993090960">
+<node TEXT="TP4056" ID="ID_1751856248" CREATED="1572993091687" MODIFIED="1572993099291"/>
+</node>
+<node TEXT="USB" ID="ID_458585926" CREATED="1572993101450" MODIFIED="1572993106660">
+<node TEXT="&#x43c;&#x438;&#x43a;&#x440;&#x43e;" ID="ID_1889778815" CREATED="1572993107607" MODIFIED="1572993124043"/>
+</node>
+</node>
+</node>
+</node>
+</map>
