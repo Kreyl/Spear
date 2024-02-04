ae1cc6e30babffdd493553040ed7dcef520e4089
kreyl <laystoll@yandex.ru>
2024-02-04 16:49:24 +0300

    Compiled

--- SpearNoRadio_fw/kl_lib/kl_lib.cpp
+++ SpearNoRadio_fw/kl_lib/kl_lib.cpp
@@ -6,10 +6,13 @@
  */
 
 #include "shell.h"
-#include <stdarg.h>
-#include <string.h>
 #include "MsgQ.h"
 #include <malloc.h>
+#include "board.h"
+#include <string>
+#include "chversion.h"
+#include "kl_lib.h"
+#include <sys/stat.h>
 
 #if 0 // ============================ General ==================================
 // To replace standard error handler in case of virtual methods implementation
@@ -69,6 +72,74 @@ void PrintMemoryInfo() {
             info.arena, info.ordblks, info.hblks, info.hblkhd,
             info.uordblks, info.fordblks, info.keepcost);
 }
+
+#if 1 // ========================== Syscalls ===================================
+// See https://sourceware.org/newlib/libc.html#Stubs
+extern "C" {
+void* _sbrk(int incr) {
+    extern uint8_t __heap_base__;
+    extern uint8_t __heap_end__;
+
+    static uint8_t *current_end = &__heap_base__;
+    uint8_t *current_block_address = current_end;
+
+    incr = (incr + 3) & (~3);
+    if(current_end + incr > &__heap_end__) {
+        errno = ENOMEM;
+        return (void*) -1;
+    }
+    current_end += incr;
+    return (void*)current_block_address;
+}
+
+int _write(int file, char *ptr, int len) { // XXX Make it good
+//    int count = len;
+//    if (file == 1) { // stdout
+//        while (count > 0) {
+//            while(!(USART1->ISR & USART_ISR_TXE));
+//            USART1->TDR = *ptr;
+//            ++ptr;
+//            --count;
+//        }
+//    }
+    return len;
+}
+
+int _close(int file) {
+    return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+int _getpid(void) {
+     return 1;
+}
+
+int _isatty(int file) {
+     return 1;
+}
+
+#undef errno
+extern int errno;
+int _kill(int pid, int sig) {
+    errno = EINVAL;
+    return -1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+    return 0;
+}
+
+int _read(int file, char *ptr, int len) {
+    return 0;
+}
+
+} // extern C
+#endif // Syscalls
+
 
 #if 1 // ============================ kl_string ================================
 __always_inline
--- SpearNoRadio_fw/main.cpp
+++ SpearNoRadio_fw/main.cpp
@@ -1,12 +1,13 @@
 #include "board.h"
-#include "led.h"
-#include "Sequences.h"
+//#include "led.h"
+//#include "Sequences.h"
 #include "kl_lib.h"
+#include "uart.h"
 #include "MsgQ.h"
 #include "SimpleSensors.h"
 #include "buttons.h"
 #include "ws2812b.h"
-#include "color.h"
+//#include "color.h"
 #include "Effects.h"
 #include "adcL151.h"
 
@@ -30,7 +31,7 @@ static const NeopixelParams_t NpxParams {NPX_SPI, NPX_DATA_PIN, NPX_DMA, NPX_DMA
 Neopixels_t Leds{&NpxParams, BAND_CNT, BAND_SETUPS};
 #endif
 
-int main(void) {
+void main(void) {
     // ==== Init Vcore & clock system ====
     SetupVCore(vcore1V8);
     // PLL fed by HSI
@@ -54,24 +55,24 @@ int main(void) {
     Clk.PrintFreqs();
 
 #if BUTTONS_ENABLED
-//    SimpleSensors::Init();
+    SimpleSensors::Init();
 #endif
 
     // ADC
-//    PinSetupOut(ADC_BAT_EN, omPushPull);
-//    PinSetHi(ADC_BAT_EN); // Enable it forever, as 200k produces ignorable current
-//    PinSetupAnalog(ADC_BAT_PIN);
-//    Adc.Init();
-//    TmrOneSecond.StartOrRestart();
+    PinSetupOut(ADC_BAT_EN, omPushPull);
+    PinSetHi(ADC_BAT_EN); // Enable it forever, as 200k produces ignorable current
+    PinSetupAnalog(ADC_BAT_PIN);
+    Adc.Init();
+    TmrOneSecond.StartOrRestart();
 
     // ==== Leds ====
-//    Leds.Init();
-//    // LED pwr pin
-//    PinSetupOut(NPX_PWR_PIN, omPushPull);
-//    PinSetHi(NPX_PWR_PIN);
+    Leds.Init();
+    // LED pwr pin
+    PinSetupOut(NPX_PWR_PIN, omPushPull);
+    PinSetHi(NPX_PWR_PIN);
 
-//    Eff::Init();
-//    Eff::FadeIn();
+    Eff::Init();
+    Eff::FadeIn();
 //    Leds.SetAll(clGreen);
 //    Leds.SetCurrentColors();
 
@@ -126,9 +127,9 @@ void OnMeasurementDone() {
         uint32_t VRef_adc = Adc.GetResultMedian(ADC_VREFINT_CHNL);
         uint32_t Vadc = Adc.GetResultMedian(BAT_CHNL);
         uint32_t Vmv = Adc.Adc2mV(Vadc, VRef_adc);
-//        Printf("VrefAdc=%u; Vadc=%u; Vmv=%u\r", VRef_adc, Vadc, Vmv);
+        Printf("VrefAdc=%u; Vadc=%u; Vmv=%u\r", VRef_adc, Vadc, Vmv);
         uint32_t Battery_mV = Vmv * 2; // Resistor divider
-//        Printf("Vbat=%u\r", Battery_mV);
+        Printf("Vbat=%u\r", Battery_mV);
         if(Battery_mV < 2800) {
             Printf("Discharged\r");
             EnterSleep();
